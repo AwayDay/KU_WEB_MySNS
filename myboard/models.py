571952b1bf22f6316fd4b99f4aadc9d116628fe6
AwayDay <awayday1108@gmail.com>
2016-03-30 18:32:28 +0900

    extend!

--- myboard/models.py
+++ myboard/models.py
@@ -8,5 +8,6 @@ class webSNS(models.Model):
     password = models.CharField(max_length=32)
     content = models.TextField()
     date = models.DateField(auto_now = True)
+    #date = models.DateTimeField(auto_now = True)
     def __unicode__(self) :
         return '%s-%s'%(self.content, self.date)
--- myboard/views.py
+++ myboard/views.py
@@ -1,15 +1,24 @@
+#-*- coding: utf-8 -*-
+
 from django.shortcuts import render
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, RequestContext
 from myboard import models
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import auth
 from .forms import PostForm
 
+from django.core import serializers
+from django.core.paginator import Paginator
+import json
+
 # Create your views here.
 
 def SnsView(request, idx):
-    docs = models.webSNS.objects.all()
+    docs = models.webSNS.objects.all().reverse()
+    #print(type(docs))
+    #print(docs.reverse())
+    PDocs = Paginator(docs, 15)
 
     if request.method == 'POST':
 
@@ -21,10 +30,12 @@ def SnsView(request, idx):
                 print(request.POST['content'])
                 form.save()
                 form = PostForm()
-                return render(request, 'timeline.html', {'form': form, 'docs' : docs})
+                return render(request, 'timeline.html', {'form': form, 'docs' : PDocs.page(1)})
+                #return render(request, 'timeline.html', {'form': form, 'docs' : docs})
             else:
                 form = PostForm()
-                return render(request, 'timeline.html', {'form': form, 'docs' : docs})
+                return render(request, 'timeline.html', {'form': form, 'docs' : PDocs.page(1)})
+                #return render(request, 'timeline.html', {'form': form, 'docs' : docs})
 
         elif 'delete' in request.POST:
             print('delete, nice!')
@@ -35,7 +46,8 @@ def SnsView(request, idx):
                 return render(request, 'del.html', {'form': form, 'doc' : doc})
             else:
                 print('somthing is wrong')
-                return render(request, 'timeline.html', {'form': form, 'docs' : docs})
+                return render(request, 'timeline.html', {'form': form, 'docs' : PDocs.page(1)})
+                #return render(request, 'timeline.html', {'form': form, 'docs' : docs})
 
         elif 'del' in request.POST:
             print('DELETE')
@@ -52,13 +64,31 @@ def SnsView(request, idx):
             else:
                 print('somthing is wrong')
             form = PostForm()
-            return render(request, 'timeline.html', {'form': form, 'docs' : docs})
+            return render(request, 'timeline.html', {'form': form, 'docs' : PDocs.page(1)})
+            #return render(request, 'timeline.html', {'form': form, 'docs' : docs})
+
+        elif request.is_ajax():
+            print('AJAX POST test')
+            page = int(request.POST.get('NumPage'))
+            #print("page: ",page)
+            if page <= PDocs.num_pages:
+                #print(PDocs.page(page).object_list)
+                data = serializers.serialize("json", PDocs.page(page).object_list)
+                return HttpResponse(data, content_type='application/json')
+                #return render(request, 'timeline.html', {'form': form, 'docs' : tmpdoc})
+            else:
+                print('fali')
+            form = PostForm()
+            return render(request, 'timeline.html', {'form': form, 'docs' : PDocs.page(1)})
+            #return render(request, 'timeline.html', {'form': form, 'docs' : docs})
 
         else:
             print('??')
             form = PostForm()
-            return render(request, 'timeline.html', {'form': form, 'docs' : docs})
+            return render(request, 'timeline.html', {'form': form, 'docs' : PDocs.page(1)})
+            #return render(request, 'timeline.html', {'form': form, 'docs' : docs})
 
     else:
         form = PostForm()
-        return render(request, 'timeline.html', {'form': form, 'docs' : docs})
+        return render(request, 'timeline.html', {'form': form, 'docs' : PDocs.page(1)})
+        #return render(request, 'timeline.html', {'form': form, 'docs' : docs})
